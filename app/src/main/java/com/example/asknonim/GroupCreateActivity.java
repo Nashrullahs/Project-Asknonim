20ddee7cbdfb3bfcaa1fdaa5d2f29f2ebed5243b
Fahmi Ardiansyah <fahmi.ardi@mhs.unsyiah.ac.id>
2020-12-17 14:34:15 +0700

    get permission from camera and gallery for group icon

--- app/src/main/java/com/example/asknonim/GroupCreateActivity.java
+++ app/src/main/java/com/example/asknonim/GroupCreateActivity.java
@@ -1,62 +1,25 @@
-<<<<<<< HEAD
 package com.example.asknonim;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-public class GroupCreateActivity extends AppCompatActivity {
-
-    //firebase auth
-    private FirebaseAuth firebaseAuth;
-
-    //action bar
-    private ActionBar actionBar;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_group_create);
-
-        actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setDisplayShowHomeEnabled(true);
-        actionBar.setTitle("Create Group");
-
-        firebaseAuth = FirebaseAuth.getInstance();
-        checkUser();
-
-    }
-
-    private void checkUser() {
-        FirebaseUser user = firebaseAuth.getCurrentUser();
-        if (user !=null){
-            actionBar.setSubtitle(user.getEmail());
-        }
-    }
-
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        onBackPressed();
-        return super.onSupportNavigateUp();
-    }
-=======
-package com.example.asknonim;
-
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
@@ -66,11 +29,11 @@ public class GroupCreateActivity extends AppCompatActivity {
 
     //permission constants
     private static final int CAMERA_REQUEST_CODE = 100;
-    private static final int STORAGE_REQUEST_CODE = 100;
+    private static final int STORAGE_REQUEST_CODE = 200;
 
     //image pick constats
-    private static final int IMAGE_PICK_CAMERA_CODE = 200;
-    private static final int IMAGE_PICK_GALLERY_CODE = 200;
+    private static final int IMAGE_PICK_CAMERA_CODE = 300;
+    private static final int IMAGE_PICK_GALLERY_CODE = 400;
 
     //permission arrays
     private String [] cameraPermissions;
@@ -90,6 +53,8 @@ public class GroupCreateActivity extends AppCompatActivity {
     private ImageView groupIconIv;
     private EditText groupTitleEt,groupDescriptionEt;
     private FloatingActionButton createGroupBtn;
+
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -126,13 +91,86 @@ public class GroupCreateActivity extends AppCompatActivity {
         createGroupBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                startCreatingGroup();
             }
         });
 
     }
 
+    private void startCreatingGroup() {
+
+    }
+
     private void showImagePickDialog() {
+        //options to pick image from
+        String [] options = {"Camera","Gallery"};
+        //dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Pick Image")
+                .setItems(options, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        //handle clicks
+                        if (which == 0){
+                            //camera clikced
+                            if (!checkCameraPermissions()){
+                                requestCameraPermissions();
+                            }
+                            else {
+                                pickFromCamera();
+                            }
+                        }
+                        else{
+                            //gallery clicked
+                            if (!checkStoragePermissions()){
+                                requestStoragePermissions();
+                            }
+                            else {
+                                pickFromGallery();
+                            }
+                        }
+                    }
+                }).show();
+    }
+
+    private void pickFromGallery(){
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent,IMAGE_PICK_GALLERY_CODE);
+    }
+
+    private void pickFromCamera(){
+        ContentValues cv = new ContentValues();
+        cv.put(MediaStore.Images.Media.TITLE,"Group Image Icon Title");
+        cv.put(MediaStore.Images.Media.DESCRIPTION,"Group Image Icon Description");
+        image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,cv);
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,image_uri);
+        startActivityForResult(intent,IMAGE_PICK_CAMERA_CODE);
+    }
+
+    private boolean checkStoragePermissions(){
+        boolean result = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+        return result;
+    }
+
+    private void requestStoragePermissions(){
+        ActivityCompat.requestPermissions(this,storagePermissions,STORAGE_REQUEST_CODE);
+    }
+
+    private boolean checkCameraPermissions(){
+        boolean result = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA) == (PackageManager.PERMISSION_GRANTED);
+        boolean result1 = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+
+        return result && result1;
+    }
+
+    private void requestCameraPermissions(){
+        ActivityCompat.requestPermissions(this,cameraPermissions,CAMERA_REQUEST_CODE);
     }
 
     private void checkUser() {
@@ -148,5 +186,60 @@ public class GroupCreateActivity extends AppCompatActivity {
         onBackPressed();
         return super.onSupportNavigateUp();
     }
->>>>>>> 1804111010061
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //handle permission result
+        switch (requestCode){
+            case CAMERA_REQUEST_CODE:{
+                if (grantResults.length >0){
+                    boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean storageAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                    if (cameraAccepted && storageAccepted){
+                        //permission allowed
+                        pickFromCamera();
+                    }
+                    else {
+                        //both or one is denied
+                        Toast.makeText(this, "Camera & Storage permissions are required", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+            break;
+            case STORAGE_REQUEST_CODE:{
+                if (grantResults.length > 0 ){
+                    boolean storageAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    if (storageAccepted){
+                        //permission allowed
+                        pickFromGallery();
+                    }
+                    else{
+                        //permissiond denied
+                        Toast.makeText(this, "Storage permissions required", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        //handle image pick result
+        if (resultCode == RESULT_OK){
+            if (requestCode == IMAGE_PICK_GALLERY_CODE){
+                //was picked from gallery
+                image_uri = data.getData();
+                //set to imageview
+                groupIconIv.setImageURI(image_uri);
+            }
+            else if (requestCode == IMAGE_PICK_CAMERA_CODE){
+                //was picked from camera
+                //set to imageview
+                groupIconIv.setImageURI(image_uri);
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 }
